31da9e33c936952055dc0b57d0a31e616c6e7d74
Vijay <vijaynatarajan@protonmail.com>
2025-02-27 15:36:16 -0500

    added middleware and authentication

--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -3,28 +3,40 @@ import { JwtPayload, jwtDecode } from 'jwt-decode';
 class AuthService {
   getProfile() {
     // TODO: return the decoded token
+    const token = this.getToken();
+    return jwtDecode<JwtPayload>(token);
   }
 
   loggedIn() {
     // TODO: return a value that indicates if the user is logged in
+    const token = this.getToken();
+    return token;
   }
   
   isTokenExpired(token: string) {
     // TODO: return a value that indicates if the token is expired
+    const exp = (JSON.parse(atob(token.split('.')[1]))).exp;
+    return (Math.floor((new Date()).getTime()/1000)) >= exp;
   }
 
   getToken(): string {
     // TODO: return the token
+    const loggedUser = localStorage.getItem('token') || '';
+    return loggedUser
   }
 
   login(idToken: string) {
     // TODO: set the token to localStorage
+    localStorage.setItem('token', idToken);
     // TODO: redirect to the home page
+    window.location.assign('/');
   }
 
   logout() {
     // TODO: remove the token from localStorage
+    localStorage.removeItem('token');
     // TODO: redirect to the login page
+    window.location.assign('/');
   }
 }
 
--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -6,7 +6,7 @@ import { authenticateToken } from '../middleware/auth.js';
 const router = Router();
 
 router.use('/auth', authRoutes);
-// TODO: Add authentication to the API routes
+// TODODONE: Add authentication to the API routes
 router.use('/api', authenticateToken, apiRoutes);
 
 export default router;
